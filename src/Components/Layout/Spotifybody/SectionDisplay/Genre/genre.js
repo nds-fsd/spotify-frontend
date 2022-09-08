0fa88bb0ecb1d50524d73cb145c75fcef94af381
Gerardo mendez <gerarditomedez.26@gmail.com>
2022-09-08 23:23:54 +0200

    link areglado

--- src/Components/Layout/Spotifybody/SectionDisplay/Genre/genre.js
+++ src/Components/Layout/Spotifybody/SectionDisplay/Genre/genre.js
@@ -6,15 +6,16 @@ const Genre = ({ name, description, photo, lin }) => {
   const [isOpen, openModal, closeModal] = useModal(false);
 
   return (
-    <button className="genre-container" onClick={openModal} type="button" href={`http://localhost:3000/genre/${lin}`}>
-      <h3 className="genre-info">{name}</h3>
-      <img src={photo} />
-      <h4 className="genre-info">{description}</h4>
-      {/* <h6>{duration}</h6>
+    <a href={`http://localhost:3000/genre/${lin}`}>
+      <button className="genre-container" onClick={openModal} type="button">
+        <h3 className="genre-info">{name}</h3>
+        <img src={photo} />
+        <h4 className="genre-info">{description}</h4>
+        {/* <h6>{duration}</h6>
       <h7>{releaseDate}</h7> */}
-      {/* <Modal isOpen={isOpen} closeModal={closeModal} name={name} description={description} phot={photo} /> */}
-      <a href={`http://localhost:3000/genre/${lin}`}>hola</a>
-    </button>
+        {/* <Modal isOpen={isOpen} closeModal={closeModal} name={name} description={description} phot={photo} /> */}
+      </button>
+    </a>
   );
 };
 
